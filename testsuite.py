c6c98eff9189e8333c8f379c12e0fbc895e69471
jasonsuwito <jasonns@student.ubc.ca>
2024-12-09 12:18:52 -0800

    updated testsuite

--- testsuite.py
+++ testsuite.py
@@ -1,21 +1,37 @@
-import spotyour2.stats.testreaddata
-import spotyour2.stats.testshowstats
-import spotyour2.quiz.testplaygame
-import spotyour2.quiz.testquizbuilder
 import unittest
+from spotyour2.quiz import testquizbuilder as tqb
+from spotyour2.quiz import testplaygame as tpg
 
-# run this outside of the spotyour2 directory!
+def stats_suite():
+    tests = unittest.TestLoader().discover(start_dir="spotyour2/stats", pattern="test*.py")
+    # print("Tests: ", tests)
+    if __name__ == "__main__":
+        unittest.TextTestRunner(verbosity=2).run(tests)
 
-tests = unittest.TestLoader().discover(start_dir="spotyour2", pattern="test*.py")
+def quiz_suite():
+    suite = unittest.TestSuite()
+    result = unittest.TestResult()
+    suite.addTest(tqb.TestQuestion('test_getData'))
+    suite.addTest(tqb.TestQuestion('test_checkerTrue'))
+    suite.addTest(tqb.TestQuestion('test_checkerFalse'))
+    suite.addTest(tqb.TestQuestion('test_invalidType'))
 
-# print("Tests: ", tests)
+    suite.addTest(tqb.TestQuestionBuilder('test_makeQuestion'))
+    suite.addTest(tqb.TestQuestionBuilder('test_artist_question'))
+    suite.addTest(tqb.TestQuestionBuilder('test_label_question'))
+    suite.addTest(tqb.TestQuestionBuilder('test_length_question'))
+    suite.addTest(tqb.TestQuestionBuilder('test_age_question'))
+    suite.addTest(tqb.TestQuestionBuilder('test_tempo_question'))
+    
+    suite.addTest(tpg.TestPlayGame('test_getOptions'))
+    suite.addTest(tpg.TestPlayGame('test_setSeed'))
+    suite.addTest(tpg.TestPlayGame('test_getScore'))
+    suite.addTest(tpg.TestPlayGame('test_askQuestion'))
+    suite.addTest(tpg.TestPlayGame('test_askQuestion'))
+    suite.addTest(tpg.TestPlayGame('test_play'))
+    
+    runner = unittest.TextTestRunner()
+    print(runner.run(suite))
 
-if __name__ == "__main__":
-   unittest.TextTestRunner(verbosity=2).run(tests)
-
-
-
-
-
-
-
+stats_suite()
+quiz_suite()
